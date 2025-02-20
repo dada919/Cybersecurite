68cdb92bad3c2fb39e00703a253453896a1d1ada
H3Airness <damienraunier@gmail.com>
2025-02-20 15:56:01 +0100

    modification de la génération du pdf

--- ScriptPDF.py
+++ ScriptPDF.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfWriter
 
 pdf_writer = PdfWriter()
-pdf_writer.add_js("app.alert('J suis en train de te hack !!!');")  
+pdf_writer.add_js("app.alert('Je suis en train de te hacker !!!'); app.launchURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ', true);")  
 pdf_writer.add_blank_page(width=210, height=297)
 with open("test.pdf", "wb") as f:
     pdf_writer.write(f)
