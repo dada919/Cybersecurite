700943e1ab286706d65288b9be3626020965c4e7
H3Airness <damienraunier@gmail.com>
2025-02-21 10:12:52 +0100

    optimisation du script de vérification de js dans le pdf

--- VerifyPDF.py
+++ VerifyPDF.py
@@ -1,18 +1,21 @@
-import PyPDF2
 import sys
 
-def test_pdf_vulnerability(pdf_file):
+def check_pdf_for_js(pdf_file):
     try:
-        with open(pdf_file, 'rb') as file:
-            reader = PyPDF2.PdfReader(file)
-            # Ici, vous pouvez ajouter des tests spécifiques pour vérifier la vulnérabilité
-            print(f"Le fichier {pdf_file} a été analysé avec succès.")
+        with open(pdf_file, "rb") as f:
+            pdf_content = f.read()
+            if b"/JavaScript" in pdf_content:
+                print("JavaScript trouvé dans le PDF.")
+            else:
+                print("Aucun JavaScript trouvé dans le PDF.")
+
     except Exception as e:
         print(f"Erreur lors de l'analyse du fichier {pdf_file}: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python ScriptPDF.py <fichier_pdf>")
+    if len(sys.argv) < 2:
+        print("Veuillez fournir le nom du fichier PDF en paramètre.")
         sys.exit(1)
-
-    test_pdf_vulnerability(sys.argv[1])
+    
+    pdf_file = sys.argv[1]
+    check_pdf_for_js(pdf_file)
